230808336ddaf68eae6c39e02fb4fd1838417aa2
marty <doc.brown.83@gmail.com>
2017-08-10 21:28:03 +0800

    Implemented IDataSource using Northwind test db.

---
+++ DataAccess/Data/Northwind.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.Data
+{
+    public class Northwind : IDataSource
+    {
+        readonly string _connString;
+
+        public Northwind(String connString)
+        {
+            _connString = connString;
+        }
+
+        public IEnumerable<EventEntry> ExecuteQuery(string query, long sequenceId)
+        {
+            using(SqlConnection conn = new SqlConnection(_connString))
+            {
+                if(conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+				using (SqlCommand cmd = new SqlCommand(query, conn))
+				{
+				    cmd.Parameters.Add(new SqlParameter("@sequenceId", sequenceId));
+				    using(SqlDataReader reader = cmd.ExecuteReader())
+				    {
+				        while(reader.Read())
+				        {
+				            yield return new EventEntry((int) reader[0], (string) reader[3]); 
+				        }
+				    }
+				}
+            }
+        }
+    }
+}
--- DataAccess/Data/SQLHelper.cs
+++ DataAccess/Data/SQLHelper.cs
@@ -8,7 +8,8 @@ namespace DataAccess.Data
 {
     public class SQLHelper
     {
-        private static string CONNECTION_STRING = @"Server=AUPERPSVSQL07;Database=EventHub.OnPrem;Trusted_Connection=True;";
+        //private static string CONNECTION_STRING = @"Server=AUPERPSVSQL07;Database=EventHub.OnPrem;Trusted_Connection=True;";
+        static string CONNECTION_STRING = @"Server=tcp:martynwind.database.windows.net,1433;Initial Catalog=Northwind;Persist Security Info=False;User ID=marty;Password=Northwind123!@#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public static string ConnectionString { get { return CONNECTION_STRING; } }
     }
 }
